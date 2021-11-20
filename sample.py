dccd7daeff1ba207fdfffa5be833d94c8b183dc5
SuzyNguyenn <hayennguyen79@gmail.com>
2021-11-20 21:31:07 +0700

    Change code
    change some code, adding saving capture pic

--- sample.py
+++ sample.py
@@ -1,29 +1,28 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import cv2
 
 st.write('Hello World')
 st.header('First Webapp')
 
 st.image('media/dog-beach-lifesaver.png')
 
-menu = ['Home','About me','Read Data']
+menu = ['Home','About me','Read Data','Capture From Webcam']
 
 choice = st.sidebar.selectbox('Menu',menu)
 
-if choice == ' Home':
+if choice == 'Home':
     st.write('Hello World')
     st.header('First Webapp')
 
-    st.image('streamlit_101/media/dog-beach-lifesaver.png')
-
-    col1,col2 = st.columns(2)
-
-    with col1:
-        dog_name = st.text_input('What is your dog name?')
-        st.write('Your dog name:',dog_name)
-    with col2:
-        age = st.slider('Dog age',min_value=1,max_value=100)
-        st.write('Your dog age:',age)
+    st.image('media/dog-beach-lifesaver.png')
+    
+    dog_name = st.text_input('What is your dog name?')
+    st.write('Your dog name:',dog_name)
+    
+    age = st.slider('Dog age',min_value=1,max_value=100)
+    st.write('Your dog age:',age)
 elif choice == 'Read Data':
     df = pd.read_csv('media/AB_NYC_2019.csv')
     st.dataframe(df)
@@ -32,5 +31,28 @@ elif choice == 'About me':
     fileUp = st.file_uploader('Upload file',type=['jpg','png','jpeg'])
     st.image(fileUp)
 
+if choice == 'Capture From Webcam':
+    cap = cv2.VideoCapture(0)  # device 0
+    run = st.checkbox('Show Webcam')
+    capture_button = st.checkbox('Capture')
 
+    captured_image = np.array(None)
+
+
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    FRAME_WINDOW = st.image([])
+    while run:
+        ret, frame = cap.read()        
+        # Display Webcam
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB ) #Convert color
+        FRAME_WINDOW.image(frame)
+
+        if capture_button:      
+            captured_image = frame
+            break
+
+    cap.release()
     
--- webcam.py
+++ webcam.py
@@ -1,6 +1,9 @@
 import cv2
+import glob
 
+i = len(glob.glob('savemoremoney/*.jpg'))
 cam = cv2.VideoCapture(0) # device 0. If not work, try with 1 or 2
+
 
 if not cam.isOpened():
     raise IOError("Cannot open webcam")
@@ -14,6 +17,9 @@ while True:
     key = cv2.waitKey(1) & 0xFF
     if key==ord("q"):
         break
+    elif key==ord("s"):
+        cv2.imwrite(f'savemoremoney/{i:03d}.jpg',frame)
+        i+=1
 
 cam.release()
 cv2.destroyAllWindows()
